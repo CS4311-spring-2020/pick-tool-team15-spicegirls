ca50856e509fcac4c98ea23226ce610923fe88cb
Dima Abdeljaber <dima.aj@icloud.com>
2020-04-01 21:47:59 -0600

    Uploaded signEvents.py

---
+++ src/signEvents.py
@@ -0,0 +1,11 @@
+class significantEvents:
+    def __init__(self):
+        self.id = None
+        self.name = ""
+        self.date = None
+        self.description = None
+        self.logEntry = None
+        self.creator = None
+        self.artifact = None
+        self.visibility = None
+        self.position = None
