45f8862f03a551441ec3acfd368d17ca387ba640
alzepeda <31897748+alzepeda@users.noreply.github.com>
2020-05-04 02:54:36 -0600

    delete redundant file

--- src/LogEntry.py
+++
@@ -1,8 +0,0 @@
-class LogEntry:
-    def __init__(self, number, timestamp, content, host, source, sourceType):
-        self.number = number
-        self.timestamp = timestamp
-        self.content = content
-        self.host = host
-        self.source = source
-        self.sourceType = sourceType
