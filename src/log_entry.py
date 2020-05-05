7163e082003a036137754e364759aa31edc6f2e7
alzepeda <31897748+alzepeda@users.noreply.github.com>
2020-05-05 03:30:23 -0600

    log_entry database methods for create, delete, and modify made and tested

--- src/log_entry.py
+++ src/log_entry.py
@@ -15,9 +15,35 @@ def create_log_entry(content, time_stamp, host, source, source_type):
 def delete_log_entry(_id):
     if db.log_entries.delete_one({'_id': ObjectId(str(_id))}).deleted_count is 1:
         return True
+    return False
 
 
 def change_log_entry_content(_id, new_content):
-    if db.log_entries.update_one({'_id': ObjectId(str(_id))},{'$set': {'content': new_content}}).modified_count is 1:
+    if db.log_entries.update_one({'_id': ObjectId(str(_id))}, {'$set': {'content': new_content}}).modified_count is 1:
         return True
+    return False
 
+
+def change_log_entry_timestamp(_id, new_timestamp):
+    if db.log_entries.update_one({'_id': ObjectId(str(_id))}, {'$set': {'time_stamp': new_timestamp}}).modified_count is 1:
+        return True
+    return False
+
+
+def change_log_entry_host(_id, new_host):
+    if db.log_entries.update_one({'_id': ObjectId(str(_id))}, {'$set': {'host': new_host}}).modified_count is 1:
+        return True
+    return False
+
+
+def change_log_entry_source(_id, new_source):
+    if db.log_entries.update_one({'_id': ObjectId(str(_id))}, {'$set': {'source': new_source}}).modified_count is 1:
+        return True
+    return False
+
+
+def change_log_entry_source_type(_id, new_source_type):
+    if db.log_entries.update_one({'_id': ObjectId(str(_id))}, {'$set': {'source_type': new_source_type}}).modified_count \
+            is 1:
+        return True
+    return False
--- src/log_entry_test.py
+++ src/log_entry_test.py
@@ -7,5 +7,13 @@ for x in range(1, 11):
 
 print(log_entry.change_log_entry_content("5eb12bfa61b2f7cd8aac1c4d", "New Content"))
 
+print(log_entry.change_log_entry_timestamp("5eb12bfa61b2f7cd8aac1c4d", "10:20:20:10:13"))
+
+print(log_entry.change_log_entry_host("5eb12bfa61b2f7cd8aac1c4d", "New Host"))
+
+print(log_entry.change_log_entry_source("5eb12bfa61b2f7cd8aac1c4d", "New Source"))
+
+print(log_entry.change_log_entry_source_type("5eb12bfa61b2f7cd8aac1c4d", "New Source Type"))
+
 #print(log_entry.delete_log_entry("5eb12bfa61b2f7cd8aac1c4c"))
 
